2220daa407c72d74729173b3d5c5a93ec2f1fbe0
alshimaa gamal <alshimaa.soger@gmail.com>
2021-04-22 13:42:33 -0700

    add lost files  payment component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { PaymentComponent } from './components/payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    PaymentComponent,
   ],
   imports: [
     BrowserModule,
