4736c0e71caae1a192af28f0a3fc5ac7b34e3511
SaraMohamed127 <msara@gmail.com>
2021-04-23 15:00:10 +0200

    create routing to products and product details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ApiProductDetailsComponent } from './components/products/api-product-details/api-product-details.component';
+import { ApiProductsComponent } from './components/products/api-products/api-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'ApiProducts',component:ApiProductsComponent},
+ {path:'ApiProduct/:id',component:ApiProductDetailsComponent},
+  {path:'',redirectTo:'/Home',pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
