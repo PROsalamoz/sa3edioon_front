67bcf538125eb37045d592da9c23da6761d7968c
mahmoud_salama <salamozm346@gmail.com>
2021-04-24 04:13:11 +0200

    add routing for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/Authentication/login/login.component';
+import { RegisterComponent } from './components/Authentication/register/register.component';
+import { ContentComponent } from './components/content/content.component';
 import { ApiProductDetailsComponent } from './components/products/api-product-details/api-product-details.component';
 import { ApiProductsComponent } from './components/products/api-products/api-products.component';
 
 const routes: Routes = [
+  { path: 'home', component: ContentComponent },
   {path:'ApiProducts',component:ApiProductsComponent},
- {path:'ApiProduct/:id',component:ApiProductDetailsComponent},
-  {path:'',redirectTo:'/Home',pathMatch:'full'},
+  {path:'ApiProduct/:id',component:ApiProductDetailsComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent},
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
