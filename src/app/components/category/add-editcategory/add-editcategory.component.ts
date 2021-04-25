d8e57ea922474c38a60290107bf2d5905546bd7c
alshimaa gamal <alshimaa.soger@gmail.com>
2021-04-25 15:51:18 -0700

    add add_editcategory component

--- src/app/components/category/add-editcategory/add-editcategory.component.ts
+++ src/app/components/category/add-editcategory/add-editcategory.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input } from '@angular/core';
+import { CategoryService } from 'src/app/services/category.service';
 @Component({
   selector: 'app-add-editcategory',
   templateUrl: './add-editcategory.component.html',
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEditcategoryComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service:CategoryService) { }
+  @Input() cat:any;
+  CategoryId:number;
+  name:string;
+  slug:string;
+  CatList:any=[];
   ngOnInit(): void {
+    this.loadCategoryList();
+  }
+  loadCategoryList(){
+    this.service.getCategoryList().subscribe((data:any)=>{
+      this.CatList=data;
+
+      this.CategoryId=this.cat.CategoryId;
+      this.name=this.cat.name;
+      this.slug=this.cat.slug;
+    });
+  }
+  addCategory(){
+    var val = {CategoryId:this.CategoryId,
+      name:this.name,
+      slug:this.slug};
+     this.service.addCategory(val).subscribe(res=>{
+      alert(res.toString());
+    });
   }
 
+  updateCategory(){
+    var val = {CategoryId:this.CategoryId,
+      name:this.name,
+      slug:this.slug
+      };
+
+    this.service.updateCategory(val).subscribe(res=>{
+    alert(res.toString());
+    });
+  }
 }
