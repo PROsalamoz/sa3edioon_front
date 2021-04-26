9af11e8d72c96d29d643ac23b0d80df713a26f04
alshimaa gamal <alshimaa.soger@gmail.com>
2021-04-26 07:16:28 -0700

    add show delivery component to show delivery persons or edit or delete delivery person

--- src/app/components/deliveryperson/showdeliveryperson/showdeliveryperson.component.ts
+++ src/app/components/deliveryperson/showdeliveryperson/showdeliveryperson.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DeliverypersonService } from 'src/app/services/deliveryperson.service';
 
 @Component({
   selector: 'app-showdeliveryperson',
@@ -7,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowdeliverypersonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Ser:DeliverypersonService) { }
+  DeliverPersonList:any=[];
 
+  ModalTitle:string;
+  ActivateAddEditDeliveryPersonComp:boolean=false;
+  deliveryperson:any;
   ngOnInit(): void {
+    this.refreshDeliveryPersonList();
+  }
+  addClick(){
+    this.deliveryperson={
+      name:"",
+      phone:"",
+      slug:"anonymous.png"
+    }
+    this.ModalTitle="Add DeliveryPerson";
+    this.ActivateAddEditDeliveryPersonComp=true;
+
   }
 
+  editClick(item){
+    console.log(item);
+    this.deliveryperson=item;
+    this.ModalTitle="Edit DeliveryPerson";
+    this.ActivateAddEditDeliveryPersonComp=true;
+  }
+
+  deleteClick(item){
+    if(confirm('Are you sure??')){
+      this.Ser.deleteDeliveryPerson(item.SubCategoryId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshDeliveryPersonList();
+      })
+    }
+  }
+
+  closeClick(){
+    this.ActivateAddEditDeliveryPersonComp=false;
+    this.refreshDeliveryPersonList();
+  }
+
+
+  refreshDeliveryPersonList(){
+    this.Ser.getDeliveryPersonList().subscribe(data=>{
+      this.DeliverPersonList=data;
+    });
+  }
 }
